82e141b1a0a23f07f00cecfd3da10a589b731fc3
七画一只妖 <1157529280@qq.com>
2021-12-27 17:19:48 +0800

    登陆界面基本完成

---
+++ src/components/login/Register.vue
@@ -0,0 +1,20 @@
+<!--
+ * @Author: 七画一只妖
+ * @Date: 2021-12-27 17:19:24
+ * @LastEditors: 七画一只妖
+ * @LastEditTime: 2021-12-27 17:19:25
+ * @Description: file content
+-->
+<template>
+  
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,28 +2,21 @@
  * @Author: 七画一只妖
  * @Date: 2021-11-17 11:42:56
  * @LastEditors: 七画一只妖
- * @LastEditTime: 2021-12-27 16:05:58
+ * @LastEditTime: 2021-12-27 16:34:46
  * @Description: file content
  */
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
-// 路由懒加载
-// const Welcome = () => import(/* webpackChunkName: "Welcome" */ '@/views/Welcome')
-// 普通用户相关组件
-// import HomePage from '@/views/HomePage'
-// import Essay from '@/views/Essay'
-// import Message from '@/views/Message'
-// import Project from '@/views/Project'
-// import About from '@/views/About'
-// import BlogInfo from '@/components/index/BlogInfo'
-// import Login from '@/components/login/Login'
+//登录界面
+import Login from '@/components/login/Login'
 
-// 管理员相关组件
-// import Admin from '@/views/Admin'
-// import UserFrom from '@/components/admin/UserFrom'
-
-const routes = []
+const routes = [
+    {
+        path: '/',
+        component: Login,
+    }
+]
 
 
 // 解决：重复点击路由报错
